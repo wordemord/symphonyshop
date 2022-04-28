5ff927dc275a4556d550abfdb6702bac263588c8
wordemord <101552412+wordemord@users.noreply.github.com>
2022-04-28 14:00:25 +0200

    product and category relations, poduct&category view forms

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Product;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,6 +19,12 @@ class ProductType extends AbstractType
             ->add('description')
             ->add('dateofcreation')
             ->add('dateofupdate')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+
+            ])
         ;
     }
 
